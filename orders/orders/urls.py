858b8d0d03424e19f2b608ad455818dbf6580f45
elharrabyassmine <elharrab.yassmine@etu.uae.ac.ma>
2026-01-15 18:12:21 +0100

    Delete orders/orders/urls.py

--- orders/orders/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("checkout/", views.checkout, name="checkout"),
-    path("success/", views.order_success, name="order_success"),
-]

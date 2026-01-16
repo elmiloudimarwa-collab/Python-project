b5d9edfb6d2f9b2d3f8ff6ac80ff4825e7629663
mfaddad <mfaddad@gmail.com>
2026-01-16 22:27:36 +0100

    cart changes

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .cart import Cart
 
 def cart_detail(request):
-    # Pour l'instant, juste une page de test
-    return render(request, "cart/detail.html")
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
+
+def cart_add(request, product_id):
+    cart = Cart(request)
+
+    # TEMPORARY price for now
+    cart.add(product_id=product_id, price=10)
+
+    return redirect('cart_detail')
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    cart.remove(product_id)
+    return redirect('cart_detail')
--- ecommerce_site/urls.py
+++ ecommerce_site/urls.py
@@ -1,37 +1,13 @@
-"""
-URL configuration for ecommerce_site project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-# URLs d’authentification Django (login, logout, etc.)
-    path('accounts/', include('django.contrib.auth.urls')),
-    
-    # pages produits (page d'accueil)
-    path('', include('products.urls')),
-
-    # panier
-    path('cart/', include('cart.urls')),
-
-    # commandes
-    path('orders/', include('orders.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # login/logout
+    path('', include('products.urls')),                     # home / product list
+    path('cart/', include('cart.urls')),                   # <-- cart URLs
+    path('orders/', include('orders.urls')),               # orders
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-

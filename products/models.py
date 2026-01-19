c2ae4d491fc833da6ecd41de3fba2a47311756c9
ilyass-sif <ilyassregi4@gmail.com>
2026-01-20 00:21:02 +0100

    product page setup

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(max_length=1024, blank=True)
+    stock = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

6861a9a6b9a25b09c4da064a56a0949f17e067a5
mfaddad <mfaddad@gmail.com>
2026-01-28 21:02:30 +0100

    cart calculations and detail

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.contrib import messages
+from products.models import Product
+from .cart import Cart
 
-# Create your views here.
+@require_POST
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product, quantity=1)
+    messages.success(request, f'✓ "{product.name}" ajouté au panier !')
+    return redirect('products:detail', product_id=product_id)
+
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    messages.success(request, f'"{product.name}" retiré du panier')
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    cart_items = []
+    for item in cart:
+        cart_items.append({
+            'product': item['product'],
+            'quantity': item['quantity'],
+            'price': item['price'],
+            'subtotal': item['total_price']
+        })
+    
+    return render(request, 'cart/cart_detail.html', {
+        'cart': cart,
+        'cart_items': cart_items,
+        'cart_total': cart.get_total_price()
+    })
